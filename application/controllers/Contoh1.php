498a02b59fa9909774663d84c478ffc6115dfcc8
Muhammad Arif <45303232+ArifSetiyawan@users.noreply.github.com>
2021-09-17 00:31:17 +0700

    Tugas-Pertemuan-1-Contoh1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -5,7 +5,9 @@ class Contoh1 extends CI_Controller {
 	public function index()
 	{
 		echo "<h1>Perkenalkan</h1>"; 
-        echo "Nama saya Arif, Saya tinggal di daerah margonda, Olah raga yang saya sukai adalah Basket";
+        echo "Nama saya Muhammad Arif Setiyawan, 
+			  Saya tinggal di daerah margonda, 
+			  Olah raga yang saya sukai adalah Basket";
 	}
 
 }
